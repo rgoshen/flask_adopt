624c595e3c522822eea8c853d7e5b74a1f600dbb
Rick Goshen <rick.goshen@gmail.com>
2021-03-20 19:30:18 -0700

    added add pet route

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 """Adopt a pet application."""
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, url_for, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_bootstrap import Bootstrap
 
 from models import db, connect_db, Pet
+from forms import AddPetForm
 
 app = Flask(__name__)
 
@@ -12,6 +13,7 @@ app.config['SECRET_KEY'] = "flyersareawesome"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///adopt_db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
 db.create_all()
@@ -26,3 +28,23 @@ def show_pets():
 
     pets = Pet.query.all()
     return render_template("index.html", pets=pets)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_pet():
+    """Add a pet."""
+
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        data = {k: v for k, v in form.data.items() if k != "csrf_token"}
+        new_pet = Pet(**data)
+        # new_pet = Pet(name=form.name.data, age=form.age.data, ...)
+        db.session.add(new_pet)
+        db.session.commit()
+        flash(f"{new_pet.name} added.")
+        return redirect(url_for('show_pets'))
+
+    else:
+        # re-present form for editing
+        return render_template("pet_add_form.html", form=form)
